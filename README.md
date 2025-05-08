1bb07bc84e45792798420adf4410b34ff10bcb9f
wood <wood@cqwod>
2025-05-08 18:43:03 +0800

    update by VS

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Toolib
+Some tool libs used by myself
--- Toolib/test.cpp
+++ Toolib/test.cpp
@@ -3,15 +3,11 @@ import WoodTools;
 
 int main()
 {
-	/*wood::bitarry str((unsigned char*)"Man", 4);
-	//std::cout << "&str:0x" << std::hex << &str << ", arryp=" << (unsigned long long)str.c_str() << std::endl;
-	wood::bitarry res = wood::ToCBase64(str);
-	res.Print(0);*/
 	char si[4] = { 'a'};
 	int ai = *((int*)si);
 	std::cout << std::hex << ai << std::endl;
 	wood::bitarry re = wood::ToCBase64(si, 1);
-	re.Print(0);
+	re.Print(0);//不是正常的Base64!
 	std::cout << "\n";
 	re.Print(1);
 	return 0;
